89f84b128976d44c96b89ae9b815c820c5e6cd8e
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-09-30 16:42:28 -0300

    exercicio_01.py

---
+++ exercicio_01/exercicio_01.py
@@ -0,0 +1,31 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class ContagemTransacoes(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_obter_pais,
+                   combiner=self.combiner_contar_transacoes,
+                   reducer=self.reducer_contar_transacoes)
+        ]
+    
+    def mapper_obter_pais(self, _, linha):
+        if linha.startswith('country_or_area'):
+            return
+        
+        campos = linha.split(";")
+        
+        pais = campos[0]
+        
+        if pais in ["Australia", "Brazil", "China"]:
+            yield (pais, 1)
+
+    def combiner_contar_transacoes(self, pais, contagens):
+        yield (pais, sum(contagens))
+
+    def reducer_contar_transacoes(self, pais, contagens):
+        yield (pais, sum(contagens))
+
+if __name__ == '__main__':
+    ContagemTransacoes.run()
