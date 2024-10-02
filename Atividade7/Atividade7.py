309c1740edd61e55fdfe914681a3b7dbadbcca70
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-10-02 15:03:07 -0300

    Atividade7.py
    Atividade 7 com código atualizado

--- Atividade7/Atividade7.py
+++ Atividade7/Atividade7.py
@@ -7,7 +7,8 @@ class CommoditiesPorFluxo(MRJob):
         return [
             MRStep(mapper=self.mapper_filtrar_transacoes,
                    combiner=self.combiner_agregar,
-                   reducer=self.reducer_encontrar_maximo)
+                   reducer=self.reducer_encontrar_maximo_fluxo),
+            MRStep(reducer=self.reducer_encontrar_maximo_por_fluxo)
         ]
 
     def mapper_filtrar_transacoes(self, _, linha):
@@ -16,27 +17,36 @@ class CommoditiesPorFluxo(MRJob):
         if colunas[0] == "country_or_area":
             return
 
-        pais = colunas[0].strip()
-        ano = int(colunas[1].strip())
-        mercadoria = colunas[3].strip()
-        fluxo = colunas[4].strip()
-        nome_quantidade = colunas[7].strip()
-        quantidade = colunas[8].strip()
+        try:
+            pais = colunas[0].strip()
+            ano = int(colunas[1].strip())
+            mercadoria = colunas[3].strip()
+            fluxo = colunas[4].strip()
+            nome_quantidade = colunas[7].strip()
+            quantidade = int(colunas[8].strip())
+        except ValueError:
+            return
 
         if pais == 'China' and ano == 2014 and nome_quantidade == 'Number of items':
-            yield (fluxo, mercadoria), int(quantidade)
+            yield (fluxo, mercadoria), quantidade
 
     def combiner_agregar(self, chave, valores):
         yield chave, sum(valores)
 
-    def reducer_encontrar_maximo(self, chave, valores):
-
+    def reducer_encontrar_maximo_fluxo(self, chave, valores):
         fluxo, mercadoria = chave
         quantidade_total = sum(valores)
+
         yield fluxo, (mercadoria, quantidade_total)
+
+    def reducer_encontrar_maximo_por_fluxo(self, fluxo, mercadorias_quantidades):
+        max_mercadoria, max_quantidade = None, 0
+
+        for mercadoria, quantidade in mercadorias_quantidades:
+            if quantidade > max_quantidade:
+                max_mercadoria, max_quantidade = mercadoria, quantidade
+
+        yield fluxo, (max_mercadoria, max_quantidade)
 
 if __name__ == '__main__':
     CommoditiesPorFluxo.run()
-
-# python .\Atividade7\Atividade7.py .\operacoes_comerciais_inteira.csv --output .\Atividade7\output\
-# cat .\Atividade7\output\* > .\Atividade7\outputAtividade7
