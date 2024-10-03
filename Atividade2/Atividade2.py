b6113bad0ce5623c27cdc95ddba703231925df1d
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-10-03 10:52:45 -0300

    Atividade2.py
    Comentários

--- Atividade2/Atividade2.py
+++ Atividade2/Atividade2.py
@@ -3,24 +3,37 @@ from mrjob.job import MRJob
 # Número de transações por ano
 
 class Transacoes(MRJob):
+
+    # Mapper
+    #Chave: Nenhuma (o identificador de chave é ignorado)
+    #Valor: Cada linha do dataset de transações
+    #Retorna: (ano, 1) se a linha não for o cabeçalho e tiver 10 campos
     def mapper(self, _, value):
         fields = value.split(';')
         if fields[0] == "country_or_area":
             return
-        if len(fields) == 10: #ignorar cabecalho
+        if len(fields) == 10:  # ignorar cabeçalho
             year = fields[1]
             yield year, 1
-            
 
+    # Combiner
+    #Chave: ano
+    #Valor: Uma lista de contagens (1s) do mapper
+    #Retorna: (ano, soma das contagens)
     def combiner(self, key, value):
         yield key, sum(value)
 
+    # Reducer
+    #Chave: ano
+    #Valor: Uma lista de contagens parciais do combiner
+    #Retorna: (ano, soma total das contagens)
     def reducer(self, key, value):
         yield key, sum(value)
 
-    
+
 if __name__ == "__main__":
     Transacoes.run()
+
     
 # python .\Atividade2\Atividade2.py .\operacoes_comerciais_inteira.csv --output .\Atividade2\output\
 # cat .\Atividade2\output\* > .\Atividade2\output_completo
