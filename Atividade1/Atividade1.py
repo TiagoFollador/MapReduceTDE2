d14e96a5049cfa2ad96799baed2c7d2f752488cf
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-10-03 10:50:13 -0300

    Atividade1.py
    Código com comentários

--- Atividade1/Atividade1.py
+++ Atividade1/Atividade1.py
@@ -9,7 +9,11 @@ class ContagemTransacoes(MRJob):
                    combiner=self.combiner_contar_transacoes,
                    reducer=self.reducer_contar_transacoes)
         ]
-    
+
+
+    #Chave: Nenhuma
+    #Valor: Cada linha do dataset de transações
+    #Retorna: (país, 1) se o país for "Australia", "Brazil" ou "China"
     def mapper_obter_pais(self, _, linha):
         if linha.startswith('country_or_area'):
             return
@@ -21,14 +25,23 @@ class ContagemTransacoes(MRJob):
         if pais in ["Australia", "Brazil", "China"]:
             yield (pais, 1)
 
+    
+    #Chave: país
+    #Valor: Uma lista de contagens de 1s do mapper
+    #Retorna: (país, soma das contagens)
     def combiner_contar_transacoes(self, pais, contagens):
         yield (pais, sum(contagens))
 
+
+    #Chave: país
+    #Valor: Uma lista de contagens parciais do combiner
+    #Retorna: (país, soma total das contagens)
     def reducer_contar_transacoes(self, pais, contagens):
         yield (pais, sum(contagens))
 
 if __name__ == '__main__':
     ContagemTransacoes.run()
+
 
 # python .\Atividade1\Atividade1.py .\operacoes_comerciais_inteira.csv --output .\Atividade1\output\
 # cat .\Atividade1\output\* > .\Atividade1\outputAtividade1
